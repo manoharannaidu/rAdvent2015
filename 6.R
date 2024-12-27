efc11f3765820b3e587f238f5882773612bd77be
manonaid <manoharan.naidu@gmail.com>
2024-12-27 16:16:09 +0530

    day 6 added optimization by vectorizing instruction parsing

--- 6.R
+++ 6.R
@@ -9,69 +9,90 @@ data <- readLines(file_path)
 grid <- matrix(0, 1000, 1000)
 
 # Helpers
-get_coords <- function(line) {
-  words <- strsplit(line, " ")[[1]]
-  coord_idx <- grep("[0-9]", words)
-  coords <- as.integer(unlist(strsplit(words[coord_idx], ","))) + 1
-  return(coords)
+preprocess_instructions <- function(data) {
+  coord_matches <- regmatches(data, gregexpr("\\d+,\\d+", data))
+  coords <- do.call(rbind, lapply(coord_matches, function(x)
+    as.integer(unlist(strsplit(
+      x, ","
+    )))))
+  
+  actions <- sub(" .*", "", data)
+  
+  list(
+    actions = actions,
+    startX = coords[, 1] + 1,
+    startY = coords[, 2] + 1,
+    endX = coords[, 3] + 1,
+    endY = coords[, 4] + 1
+  )
 }
 
+instructions <- preprocess_instructions(data)
+
 # Part 1
-process_instructions <- function(line, grid) {
-  coords <- get_coords(line)
-  startX <- coords[1]
-  startY <- coords[2]
-  endX <- coords[3]
-  endY <- coords[4]
-  if (grepl("turn off", line, fixed = T)) {
-    grid[startX:endX, startY:endY] <- 0
-  } else if (grepl("turn on", line, fixed = T)) {
-    grid[startX:endX, startY:endY] <- 1
-  } else {
-    grid[startX:endX, startY:endY] <- !grid[startX:endX, startY:endY]
+apply_instructions <- function(data, grid, instructions) {
+  actions <- instructions$actions
+  startX <- instructions$startX
+  startY <- instructions$startY
+  endX <- instructions$endX
+  endY <- instructions$endY
+  
+  for (i in seq_along(actions)) {
+    subgrid <- grid[startX[i]:endX[i], startY[i]:endY[i]]
+    if (actions[i] == "turn") {
+      if (grepl("off", data[i])) {
+        grid[startX[i]:endX[i], startY[i]:endY[i]] <- 0
+      } else {
+        grid[startX[i]:endX[i], startY[i]:endY[i]] <- 1
+      }
+    } else if (actions[i] == "toggle") {
+      grid[startX[i]:endX[i], startY[i]:endY[i]] <- !subgrid
+    }
   }
   grid
 }
 
-part1 <- function(data, G) {
-  for (line in data) {
-    G <- process_instructions(line, G)
-  }
+part1 <- function(data, G, instructions) {
+  G <- apply_instructions(data, G, instructions)
   sum(G)
 }
 
 # Part 2
-process_instructions_v2 <- function(line, grid) {
-  coords <- get_coords(line)
-  startX <- coords[1]
-  startY <- coords[2]
-  endX <- coords[3]
-  endY <- coords[4]
-  if (grepl("turn off", line, fixed = T)) {
-    grid[startX:endX, startY:endY] <- pmax(grid[startX:endX, startY:endY] - 1, 0)
-  } else if (grepl("turn on", line, fixed = T)) {
-    grid[startX:endX, startY:endY] <- grid[startX:endX, startY:endY] + 1
-  } else {
-    grid[startX:endX, startY:endY] <- grid[startX:endX, startY:endY] + 2
+apply_instructions_v2 <- function(data, grid, instructions) {
+  actions <- instructions$actions
+  startX <- instructions$startX
+  startY <- instructions$startY
+  endX <- instructions$endX
+  endY <- instructions$endY
+  
+  for (i in seq_along(actions)) {
+    subgrid <- grid[startX[i]:endX[i], startY[i]:endY[i]]
+    if (actions[i] == "turn") {
+      if (grepl("off", data[i])) {
+        grid[startX[i]:endX[i], startY[i]:endY[i]] <- pmax(subgrid - 1, 0)
+      } else {
+        grid[startX[i]:endX[i], startY[i]:endY[i]] <- subgrid + 1
+      }
+    } else if (actions[i] == "toggle") {
+      grid[startX[i]:endX[i], startY[i]:endY[i]] <- subgrid + 2
+    }
   }
   grid
 }
 
-part2 <- function(data, G) {
-  for (line in data) {
-    G <- process_instructions_v2(line, G)
-  }
-  sum(G)
+part2 <- function(data, grid, instructions) {
+  grid <- apply_instructions_v2(data, grid, instructions)
+  
+  sum(grid)
 }
 
-
 # Run
-part1(data, grid)
-part2(data, grid)
+part1(data, grid, instructions)
+part2(data, grid, instructions)
 
 # Benchmarking
 microbenchmark(
-  part1 = part1(data, grid),
-  part2 = part2(data, grid),
-  times = 5
+  part1 = part1(data, grid, instructions),
+  part2 = part2(data, grid, instructions),
+  times = 100
 )
